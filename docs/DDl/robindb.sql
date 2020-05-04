3be4754af87736e224fd272c837123bbeb2bcff8
Nikita <43067463+nikitalukash01@users.noreply.github.com>
2020-05-04 19:45:23 +0300

    fix: SQL generation script

--- docs/DDl/robindb.sql
+++ docs/DDl/robindb.sql
@@ -21,12 +21,12 @@ USE `robindb` ;
 -- Table `robindb`.`user`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `robindb`.`user` (
-  `user_id` VARCHAR(36) NOT NULL,
-  `status` VARCHAR(30) NOT NULL,
-  `name` VARCHAR(20) NOT NULL,
+  `user_id` varchar (255) NOT NULL,
+  `status` VARCHAR(50) NOT NULL,
+  `name` VARCHAR(30) NOT NULL,
   `display_name` VARCHAR(50) NOT NULL,
-  `email` VARCHAR(40) NULL DEFAULT NULL,
-  `password` VARCHAR(60) NULL DEFAULT NULL,
+  `email` VARCHAR(100) NULL DEFAULT NULL,
+  `password` VARCHAR(100) NULL DEFAULT NULL,
   `ispro` TINYINT(1) NOT NULL DEFAULT '0',
   `created_at` DATETIME NULL DEFAULT NULL,
   `updated_at` DATETIME NULL DEFAULT NULL,
@@ -40,7 +40,7 @@ COLLATE = utf8mb4_0900_ai_ci;
 -- Table `robindb`.`service`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `robindb`.`service` (
-  `service_id` VARCHAR(36) NOT NULL,
+  `service_id` varchar(255) NOT NULL,
   `name` VARCHAR(20) NULL DEFAULT NULL,
   `description` VARCHAR(36) NOT NULL,
   `created_at` DATETIME NULL DEFAULT NULL,
@@ -55,18 +55,19 @@ COLLATE = utf8mb4_0900_ai_ci;
 -- Table `robindb`.`develop`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `robindb`.`develop` (
-  `develop_id` VARCHAR(36) NOT NULL,
+  `developer_id` varchar(255) NOT NULL,
   `developer_type` VARCHAR(50) NULL DEFAULT NULL,
   `promoted_by` VARCHAR(36) NULL DEFAULT NULL,
   `api_token` VARCHAR(36) NULL DEFAULT NULL,
-  PRIMARY KEY (`develop_id`),
+	`service_id` varchar(255) null default null,
   INDEX `promoted_by` (`promoted_by` ASC) VISIBLE,
   INDEX `api_token` (`api_token` ASC) VISIBLE,
   CONSTRAINT `develop_ibfk_1`
     FOREIGN KEY (`promoted_by`)
     REFERENCES `robindb`.`user` (`user_id`),
-  CONSTRAINT `develop_ibfk_2`
-    FOREIGN KEY (`api_token`)
+	FOREIGN KEY (`developer_id`)
+    REFERENCES `robindb`.`user` (`user_id`),
+    FOREIGN KEY (`service_id`)
     REFERENCES `robindb`.`service` (`service_id`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
@@ -77,16 +78,20 @@ COLLATE = utf8mb4_0900_ai_ci;
 -- Table `robindb`.`owner`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `robindb`.`owner` (
-  `owner_id` VARCHAR(36) NOT NULL,
   `ownership_type` VARCHAR(20) NULL DEFAULT NULL,
-  `base_id` VARCHAR(36) NULL DEFAULT NULL,
+  `base_id` varchar(255) NULL DEFAULT NULL,
+  `user_id` varchar(255) NOT NULL,
   `promoted_by` VARCHAR(36) NULL DEFAULT NULL,
   `promoted_at` DATETIME NULL DEFAULT NULL,
-  PRIMARY KEY (`owner_id`),
   INDEX `promoted_by` (`promoted_by` ASC) VISIBLE,
   CONSTRAINT `owner_ibfk_1`
-    FOREIGN KEY (`promoted_by`)
-    REFERENCES `robindb`.`user` (`user_id`))
+	FOREIGN KEY(`base_id`) 
+	REFERENCES `robindb`.`OpenData` (`data_id`),
+	 FOREIGN KEY (`promoted_by`)
+    REFERENCES `robindb`.`user` (`user_id`),
+	 FOREIGN KEY (`user_id`)
+    REFERENCES `robindb`.`user` (`user_id`)
+)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
@@ -96,22 +101,19 @@ COLLATE = utf8mb4_0900_ai_ci;
 -- Table `robindb`.`opendata`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `robindb`.`opendata` (
-  `data_id` VARCHAR(36) NOT NULL,
-  `adress` VARCHAR(50) NOT NULL,
-  `data_type` VARCHAR(20) NULL DEFAULT NULL,
+  `data_id` varchar(255) NOT NULL,
+  `adress` VARCHAR(100) NOT NULL,
+  `data_type` VARCHAR(50) NULL DEFAULT NULL,
   `data_title` VARCHAR(35) NULL DEFAULT NULL,
   `description` TEXT NULL DEFAULT NULL,
-  `version` VARCHAR(50) NULL DEFAULT NULL,
-  `used_by` VARCHAR(36) NULL DEFAULT NULL,
-  `total_rows` BIGINT UNSIGNED NULL DEFAULT NULL,
-  `issues` TEXT NULL DEFAULT NULL,
+  `version` VARCHAR(15) NULL DEFAULT NULL,
+  `used_by` int  NULL DEFAULT NULL,
+  `total_rows` int  NULL DEFAULT NULL,
   `first_publish` DATETIME NULL DEFAULT NULL,
   `last_publish` DATETIME NULL DEFAULT NULL,
   PRIMARY KEY (`data_id`),
-  INDEX `used_by` (`used_by` ASC) VISIBLE,
-  CONSTRAINT `opendata_ibfk_1`
-    FOREIGN KEY (`used_by`)
-    REFERENCES `robindb`.`owner` (`owner_id`))
+  INDEX `used_by` (`used_by` ASC) VISIBLE
+  )
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
